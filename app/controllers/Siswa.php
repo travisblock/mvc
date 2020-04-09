959856f2b88a0aaedcd205a82dabdce7e601fd45
SQL47 <ajidalmajid6@gmail.com>
2020-04-10 01:39:18 +0700

    update edit and delete

--- app/controllers/Siswa.php
+++ app/controllers/Siswa.php
@@ -3,7 +3,8 @@
 class Siswa extends Controller{
 
 	private $validasi,
-					$eror = array();
+					$eror = array(),
+					$errorrs = null;
 
 	public function __construct(){
 
@@ -29,21 +30,62 @@ class Siswa extends Controller{
 
 	public function tambah(){
 		$nama = Input::get('nama');
-		$this->cekValidasi($nama);
 		if($this->cekValidasi($nama)){
 			if($this->model('Siswa_model')->tambahSiswa(Input::get()) > 0){
-				Msg::setMSG('Data siswa berhasil ditambahkan', 'success');
+				Msg::setMSG('Data siswa sukses ditambahkan', 'success');
 				header('Location:'. BASEURL . 'siswa');
 				exit();
 			}
+
 		}else{
+
 			$errArr = array_unique($this->eror);
 
-			foreach ($errArr as $err) {
+			for($i=0; $i < count($errArr); $i++){
+				echo $errArr[$i];
+				Msg::setMSG($errArr[$i], 'danger');
 
-				Msg::setErrMsg($err);
+				header('Location:'. BASEURL . 'siswa');
+				exit();
 
 			}
+
+		}
+	}
+
+
+	public function ubah(){
+		$nama = Input::get('nama');
+		if($this->cekValidasi($nama)){
+			if($this->model('Siswa_model')->ubahSiswa(Input::get()) > 0){
+				Msg::setMSG('Data siswa sukses diubah', 'success');
+				header('Location:'. BASEURL . 'siswa');
+				exit();
+			}
+
+		}else{
+
+			$errArr = array_unique($this->eror);
+
+			for($i=0; $i < count($errArr); $i++){
+				echo $errArr[$i];
+				Msg::setMSG($errArr[$i], 'danger');
+
+				header('Location:'. BASEURL . 'siswa');
+				exit();
+
+			}
+
+		}
+	}
+
+	public function delete($id){
+			if($this->model('Siswa_model')->deleteSiswa($id) > 0){
+				Msg::setMSG('Data siswa sukses dihapus', 'success');
+				header('Location:'. BASEURL . 'siswa');
+				exit();
+		}else{
+			Msg::setMSG('Data siswa gagal dihapus', 'danger');
 			header('Location:'. BASEURL . 'siswa');
 			exit();
 		}
@@ -57,37 +99,50 @@ class Siswa extends Controller{
 									'max'			 => 50
 								),
 			'kelas' => array(
-									'required' => true,
+									'required' => true
 								),
 			'jurusan' => array(
-									'required' => true,
+									'required' => true
 								),
 			'email' => array(
-									'required' => true,
+									'required' => true
 								)
 		));
 	return $validation;
 	}
 
 	public function cekValidasi($nama=null){
-			if($this->cekDataSiswa($nama)){
-				if($this->validasiUser()->passed()){
-					return true;
-				}else{
-					foreach ($this->validasiUser()->errors() as $erorr) {
-						$this->eror[] = $erorr;
-					}
+		// var_dump($this->cekDataSiswa($nama));
+		if(is_null($this->cekDataSiswa($nama))){
+			if($this->validasiUser()->passed()){
+				return true;
+
+			}else{
+				foreach ($this->validasiUser()->errors() as $erorr) {
+					$this->eror[] = $erorr;
 				}
+				return false;
+			}
+
+		}else{
+			foreach ($this->validasiUser()->errors() as $erorr) {
+				$this->eror[] = $erorr;
 			}
 			return false;
+		}
+
 	}
 
 	public function cekDataSiswa($nama = null){
-		if($this->model('Siswa_model')->siswaExists($nama) < 1){
-			return true;
-		}else{
-			$this->eror[] = "Nama tidak boleh sama";
-			return $this->eror;
+		if($this->model('Siswa_model')->siswaExists($nama) > 0){
+			return $this->eror[] = "Nama tidak boleh sama";
 		}
 	}
+
+	public function getUbah(){
+		echo json_encode($this->model('Siswa_model')->getSiswaById(Input::get('id')));
+	}
+
+
+
 }
