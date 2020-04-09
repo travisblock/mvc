6e87cd2cc1412a24d1822233de4bcd1e80a4b4ae
SQL47 <ajidalmajid6@gmail.com>
2020-04-10 01:41:04 +0700

    add link to script.js

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -8,5 +8,6 @@
 	<script> window.setTimeout(function() { $(".alert-msg").fadeTo(500, 0).slideUp(500, function(){ $(this).remove(); }); }, 3000); </script>
 	<script src="<?= BASEURL; ?>public/js/jquery.min.js"></script>
 	<script src="<?= BASEURL; ?>public/js/bootstrap.min.js"></script>
+	<script src="<?= BASEURL; ?>public/js/script.js"></script>
 </body>
 </html>
