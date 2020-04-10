edccf14ed492ef2566f7957227d69624fac27d50
SQL47 <ajidalmajid6@gmail.com>
2020-04-10 23:22:19 +0700

    add link to cari.js

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -9,5 +9,6 @@
 	<script src="<?= BASEURL; ?>public/js/jquery.min.js"></script>
 	<script src="<?= BASEURL; ?>public/js/bootstrap.min.js"></script>
 	<script src="<?= BASEURL; ?>public/js/script.js"></script>
+	<script src="<?= BASEURL; ?>public/js/cari.js"></script>
 </body>
 </html>
